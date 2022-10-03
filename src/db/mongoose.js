f0343ec98ac6831a13fc9cfb88158a7c12e9cf67
Septian Dika Maulana <C2318G2774@bangkit.academy>
2022-10-04 00:49:13 +0700

    .

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URL).then(() => {
+mongoose.connect(process.env.MONGODB_URL || "mongodb+srv://TaskApp:Septian12345@cluster0.zfih1sk.mongodb.net/task-manager-api?retryWrites=true"
+).then(() => {
     console.log('MongoDB successfully connected. Database URL:', process.env.MONGODB_URL);
 }).catch(error => {
     console.log('Error establishing MongoDB connection. Error:', error.message);
